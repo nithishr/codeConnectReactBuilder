3acb46ca3e4e102b80df0c27d42a0606d60bb149
Nithish R <nithishr@gmail.com>
2016-08-29 21:17:34 +0200

    Updated from old Repo

--- server.js
+++ server.js
@@ -1,20 +1,136 @@
+var fs = require('fs');
 var path = require('path');
 var express = require('express');
 var rewrite = require('express-urlrewrite');
+var passport = require('passport');
+var util = require('util');
+var session = require('express-session');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+var GitHubStrategy = require('passport-github2').Strategy;
+var partials = require('express-partials');
+
+
+var GITHUB_CLIENT_ID = "c40cd4a33d5dfe4aa2fd";
+var GITHUB_CLIENT_SECRET = "6cd5742c089339bad6d2600fe426350430f37ed0";
+
+
+// Passport session setup.
+//   To support persistent login sessions, Passport needs to be able to
+//   serialize users into and deserialize users out of the session.  Typically,
+//   this will be as simple as storing the user ID when serializing, and finding
+//   the user by ID when deserializing.  However, since this example does not
+//   have a database of user records, the complete GitHub profile is serialized
+//   and deserialized.
+passport.serializeUser(function(user, done) {
+    done(null, user);
+});
+
+passport.deserializeUser(function(obj, done) {
+    done(null, obj);
+});
+
+
+// Use the GitHubStrategy within Passport.
+//   Strategies in Passport require a `verify` function, which accept
+//   credentials (in this case, an accessToken, refreshToken, and GitHub
+//   profile), and invoke a callback with a user object.
+passport.use(new GitHubStrategy({
+        clientID: GITHUB_CLIENT_ID,
+        clientSecret: GITHUB_CLIENT_SECRET,
+        callbackURL: "http://127.0.0.1:3000/auth/github/callback"
+    },
+    function(accessToken, refreshToken, profile, done) {
+        // asynchronous verification, for effect...
+        process.nextTick(function() {
+
+            // To keep the example simple, the user's GitHub profile is returned to
+            // represent the logged-in user.  In a typical application, you would want
+            // to associate the GitHub account with a user record in your database,
+            // and return that user instead.
+            return done(null, profile);
+        });
+    }
+));
+
+
 
 var app = express();
 
 app.set('port', (process.env.PORT || 3000));
+
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+app.get('/account', ensureAuthenticated, function(req, res) {
+    res.render('account', {
+        user: req.user
+    });
+});
+
+app.get('/login', function(req, res) {
+    res.render('login', {
+        user: req.user
+    });
+});
+
+app.get('/auth/github',
+    passport.authenticate('github', {
+        scope: ['user:email']
+    }),
+    function(req, res) {
+        // The request will be redirected to GitHub for authentication, so this
+        // function will not be called.
+    });
+
+app.get('/auth/github/callback',
+    passport.authenticate('github', {
+        failureRedirect: '/login'
+    }),
+    function(req, res) {
+        res.redirect('/');
+    });
+
+app.get('/logout', function(req, res) {
+    req.logout();
+    res.redirect('/');
+});
+
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) { return next(); }
+  res.redirect('/login')
+}
+
+////////////////////////////////////////////////////77
+
+////////////////////////////////////////////////////////
+app.get('/task', function(req, res) {
+    res.send("Task_JSON");
+});
+
+app.get('/project', function(req, res) {
+    res.send("Project_JSON");
+});
+
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+app.use(bodyParser.json());
+
+app.post('/task', function(req, res) {
+    console.log(req.body);
+    res.send("works");
+});
 
 app.use('/resources', express.static(path.join(__dirname, 'public/resources')));
 app.use('/images', express.static(path.join(__dirname, 'public/images')));
 app.use(rewrite('/*', '/index.html'));
 app.use('/', express.static(path.join(__dirname, 'public')));
 
-app.get('/hallo', function(req, res){
-res.send("sfdsdfas");
-});
+
 
 app.listen(app.get('port'), function() {
-  console.log('Server started: http://localhost:' + app.get('port') + '/');
+    console.log('Server started: http://localhost:' + app.get('port') + '/');
 });
